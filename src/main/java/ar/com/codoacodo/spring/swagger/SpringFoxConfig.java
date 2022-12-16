c3494cdf54fd7978029b1a9f28a552c4824b7f4c
lionel staricoff <staricofflionel@hotmail.com>
2022-12-16 03:05:09 -0500

    se termina manejo de errores con la ui de swagger

--- src/main/java/ar/com/codoacodo/spring/swagger/SpringFoxConfig.java
+++ src/main/java/ar/com/codoacodo/spring/swagger/SpringFoxConfig.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
@@ -25,42 +27,88 @@ public class SpringFoxConfig {
 
 	@Bean
 	public Docket createRestApi() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-				.paths(PathSelectors.any())
-				.build()
-				.securityContexts(securityContext())
-				.securitySchemes(securitySchemes())
-				;
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build().securityContexts(securityContext()).securitySchemes(securitySchemes())
+				.useDefaultResponseMessages(false)
+				// seteo interfaz ui swagger para errores
+				.globalResponses(HttpMethod.GET,
+						List.of(new ResponseBuilder().code("200").description("200 OK!!!").build(),
+								new ResponseBuilder().code("201").description("201 CREATED").build(),
+								new ResponseBuilder().code("204").description("204 NO CONTENT").build(),
+								new ResponseBuilder().code("400").description("400 BAD REQUEST").build(),
+								new ResponseBuilder().code("401").description("401 UNAUTHORIZED!!!").build(),
+								new ResponseBuilder().code("403").description("Forbidden!!!!!").build(),
+								new ResponseBuilder().code("404").description("404 NOT FOUND!!!!!").build(),
+								new ResponseBuilder().code("405").description("405 METHOD NOT ALLOWED!!!!!").build(),
+								new ResponseBuilder().code("406").description("406 NOT ACCEPTABLE!!!!!").build(),
+								new ResponseBuilder().code("409").description("409 CONFLICT!!!").build(),
+								new ResponseBuilder().code("415").description("415 UNSUPPORTED MEDIA TYPE!!").build(),
+								new ResponseBuilder().code("500").description("500 INTERNAL SERVER ERROR!!").build()))
+				.globalResponses(HttpMethod.POST,
+						List.of(new ResponseBuilder().code("200").description("200 OK!!!").build(),
+								new ResponseBuilder().code("201").description("201 CREATED").build(),
+								new ResponseBuilder().code("204").description("204 NO CONTENT").build(),
+								new ResponseBuilder().code("400").description("400 BAD REQUEST").build(),
+								new ResponseBuilder().code("401").description("401 UNAUTHORIZED!!!").build(),
+								new ResponseBuilder().code("403").description("Forbidden!!!!!").build(),
+								new ResponseBuilder().code("404").description("404 NOT FOUND!!!!!").build(),
+								new ResponseBuilder().code("405").description("405 METHOD NOT ALLOWED!!!!!").build(),
+								new ResponseBuilder().code("406").description("406 NOT ACCEPTABLE!!!!!").build(),
+								new ResponseBuilder().code("409").description("409 CONFLICT!!!").build(),
+								new ResponseBuilder().code("415").description("415 UNSUPPORTED MEDIA TYPE!!").build(),
+								new ResponseBuilder().code("500").description("500 INTERNAL SERVER ERROR!!").build()))
+				.globalResponses(HttpMethod.PUT,
+						List.of(new ResponseBuilder().code("200").description("200 OK!!!").build(),
+								new ResponseBuilder().code("201").description("201 CREATED").build(),
+								new ResponseBuilder().code("204").description("204 NO CONTENT").build(),
+								new ResponseBuilder().code("400").description("400 BAD REQUEST").build(),
+								new ResponseBuilder().code("401").description("401 UNAUTHORIZED!!!").build(),
+								new ResponseBuilder().code("403").description("Forbidden!!!!!").build(),
+								new ResponseBuilder().code("404").description("404 NOT FOUND!!!!!").build(),
+								new ResponseBuilder().code("405").description("405 METHOD NOT ALLOWED!!!!!").build(),
+								new ResponseBuilder().code("406").description("406 NOT ACCEPTABLE!!!!!").build(),
+								new ResponseBuilder().code("409").description("409 CONFLICT!!!").build(),
+								new ResponseBuilder().code("415").description("415 UNSUPPORTED MEDIA TYPE!!").build(),
+								new ResponseBuilder().code("500").description("500 INTERNAL SERVER ERROR!!").build()))
+				.globalResponses(HttpMethod.DELETE,
+						List.of(new ResponseBuilder().code("200").description("200 OK!!!").build(),
+								new ResponseBuilder().code("201").description("201 CREATED").build(),
+								new ResponseBuilder().code("204").description("204 NO CONTENT").build(),
+								new ResponseBuilder().code("400").description("400 BAD REQUEST").build(),
+								new ResponseBuilder().code("401").description("401 UNAUTHORIZED!!!").build(),
+								new ResponseBuilder().code("403").description("Forbidden!!!!!").build(),
+								new ResponseBuilder().code("404").description("404 NOT FOUND!!!!!").build(),
+								new ResponseBuilder().code("405").description("405 METHOD NOT ALLOWED!!!!!").build(),
+								new ResponseBuilder().code("406").description("406 NOT ACCEPTABLE!!!!!").build(),
+								new ResponseBuilder().code("409").description("409 CONFLICT!!!").build(),
+								new ResponseBuilder().code("415").description("415 UNSUPPORTED MEDIA TYPE!!").build(),
+								new ResponseBuilder().code("500").description("500 INTERNAL SERVER ERROR!!").build()));
+
 	}
-	
+
 	private List<SecurityScheme> securitySchemes() {
 		ApiKey jwt = new ApiKey("JWT", "Authorization", "header");
 		return Collections.singletonList(jwt);
 	}
-	
+
 	public List<SecurityContext> securityContext() {
-		SecurityContext securityContext = SecurityContext.builder()
-				.securityReferences(defautlAuth())
-				.build();
+		SecurityContext securityContext = SecurityContext.builder().securityReferences(defautlAuth()).build();
 		return Collections.singletonList(securityContext);
 	}
-	
+
 	private List<SecurityReference> defautlAuth() {
-		AuthorizationScope authorizationScope = new AuthorizationScope("global","accessEverithing");
-		AuthorizationScope[] authorizationsScopes = new AuthorizationScope[] {authorizationScope};		
-		SecurityReference securityReference = new SecurityReference("JWT",authorizationsScopes);
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverithing");
+		AuthorizationScope[] authorizationsScopes = new AuthorizationScope[] { authorizationScope };
+		SecurityReference securityReference = new SecurityReference("JWT", authorizationsScopes);
 		return Collections.singletonList(securityReference);
 	}
-	
+
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
-				.contact(new Contact("Lionel Staricoff", "https://github.com/LionelStaricoff/api-rest-spring-boot", "staricofflionel@hotmail.com"))
-				.description("Proyecto Rest final, Bootcamp")
-				.title("SpringBoot + JWT + Swagger ")
-				.version("1.0.0-FINAL")
-				.build();
+				.contact(new Contact("Lionel Staricoff", "https://github.com/LionelStaricoff/api-rest-spring-boot",
+						"staricofflionel@hotmail.com"))
+				.description("Proyecto Rest final, Bootcamp Codo a Codo").title("SpringBoot + JWT + Swagger ")
+				.version("1.0.0-FINAL").build();
 	}
 }
